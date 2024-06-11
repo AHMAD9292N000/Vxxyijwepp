674b0981842467f7f8eb1c53da55e9c64d8037bb
AHMAD9292N000 <ahamdiwowbsk@gmail.com>
2024-06-12 01:22:11 +0200

    Update play.py

--- MatrixMusic/utils/inline/play.py
+++ MatrixMusic/utils/inline/play.py
@@ -17,7 +17,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
                 callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
             ),
         ],
-        [InlineKeyboardButton(text=_["MATRIX_BUTTON"], url=f"https://t.me/vvizinn")],
+        [InlineKeyboardButton(text=_["MATRIX_BUTTON"], url=f"https://t.me/CZCRR")],
         [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
